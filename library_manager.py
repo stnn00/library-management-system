5d6d8e69216cb063b9427ed51852f3d4898ad799
stnn00 <nguyens6@csp.edu>
2025-09-19 00:47:36 -0500

    Implement: add_book(library) function to add new Book objects to the library list parameter

--- library_manager.py
+++ library_manager.py
@@ -3,3 +3,17 @@ library_manager.py
 """
 
 from book import Book
+
+def add_book(library):
+    """
+    Prompts user to input book details and adds a new Book object to the library.
+
+    Parameters:
+    library (list): List of Book objects in the library
+    """
+    title = input("Enter in the title of the book: ")
+    author = input("Enter in the author of the book: ")
+    isbn = input("Enter in the ISBN of the book: ")
+
+    new_book = Book(title, author, isbn)
+    library.append(new_book)
